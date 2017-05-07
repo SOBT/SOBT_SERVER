f75ec1be8b4f6e891a90e98917b28e1cfed8cd32
Song Intae <Song@Songui-MacBook-Pro.local>
2017-05-07 19:16:12 +0900

    1.Domain, Dao  패키지 생성 2.User  Domain 생성 3.ERD 수정

---
+++ src/main/java/sobt/dao/user/UserDao.java
@@ -0,0 +1,13 @@
+package sobt.dao.user;
+
+import sobt.domain.user.User;
+
+public interface UserDao {
+	public void addUser(User user);
+	public User getUser(String userId);
+	
+	
+	//테스트용..
+	public int getCount();
+	public void deleteAll();
+}
