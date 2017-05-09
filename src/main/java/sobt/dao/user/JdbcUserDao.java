5aed601b2b8d456abccce22cb157234cdca0ba5c
Song Intae <Song@Songui-MacBook-Pro.local>
2017-05-09 18:12:38 +0900

    UserDao 생성..

---
+++ src/main/java/sobt/dao/user/JdbcUserDao.java
@@ -0,0 +1,77 @@
+package sobt.dao.user;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import sobt.domain.user.User;
+import sobt.sql.service.SqlService;
+
+public class JdbcUserDao implements UserDao {
+	private JdbcTemplate jdbcTemplate;
+	private SqlService sqlService;
+	
+	private RowMapper<User> userMapper = new RowMapper<User>(){
+
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			// TODO Auto-generated method stub
+			User user = new User();
+			user.setUserId(rs.getString("user_id"));
+			return user;
+		}};
+	
+	public void setDataSource(DataSource dataSource){
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public void setSqlService(SqlService sqlService){
+		this.sqlService = sqlService;
+	}
+	
+	
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		try{
+			this.jdbcTemplate.update(this.sqlService.getSql("addUser"),user.getUserId());
+		}catch(DuplicateKeyException e){
+			//log 처리 
+		}
+		
+	}
+
+	@Override
+	public User getUser(String userId) {
+		// TODO Auto-generated method stub
+		return this.jdbcTemplate.queryForObject(this.sqlService.getSql("getUser"),new Object[]{userId},this.userMapper);
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return this.jdbcTemplate.query(this.sqlService.getSql("getCount"), new ResultSetExtractor<Integer>(){
+
+			@Override
+			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+				// TODO Auto-generated method stub
+				rs.next();
+				return rs.getInt(1);
+			}});
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		this.jdbcTemplate.update(this.sqlService.getSql("deleteAll"));
+		
+	}
+
+}
---
+++ src/test/java/com/sobt/project/dao/test/UserDaoTest.java
@@ -0,0 +1,93 @@
+package com.sobt.project.dao.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import sobt.dao.user.UserDao;
+import sobt.domain.user.User;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/test-applicationContext.xml")
+public class UserDaoTest {
+	
+	@Autowired
+	UserDao userDao;
+	
+	User user1;
+	User user2;
+	User user3;
+	
+	@Before
+	public void setUp(){
+		userDao.deleteAll();
+		user1 = new User("testUser1");
+		user2 = new User("testUser2");
+		user3 = new User("testUser3");
+	}
+	
+	@Test
+	public void addUser(){
+		assertThat(userDao.getCount(), is(0));
+		
+		userDao.addUser(user1);
+		assertThat(userDao.getCount(), is(1));
+		
+		userDao.addUser(user2);
+		assertThat(userDao.getCount(), is(2));
+		
+		userDao.addUser(user3);
+		assertThat(userDao.getCount(), is(3));
+		
+	}
+	
+	@Test
+	public void getUser(){
+		assertThat(userDao.getCount(), is(0));
+		
+		userDao.addUser(user1);
+		assertThat(userDao.getCount(), is(1));
+		
+		userDao.addUser(user2);
+		assertThat(userDao.getCount(), is(2));
+		
+		userDao.addUser(user3);
+		assertThat(userDao.getCount(), is(3));
+		
+		User getUser1 = userDao.getUser(user1.getUserId());
+		checkSameUser(user1,getUser1);
+		
+		User getUser2 = userDao.getUser(user2.getUserId());
+		checkSameUser(user2,getUser2);
+		
+		User getUser3 = userDao.getUser(user3.getUserId());
+		checkSameUser(user3,getUser3);
+		
+		
+	}
+	
+	private void checkSameUser(User user1 , User user2){
+		assertThat(user1.getUserId(),is(user2.getUserId()));
+	}
+	
+	
+	@Test//(expected=DuplicateKeyException.class)
+	public void addUserDuplicatekeyException(){
+		userDao.addUser(user1);
+		userDao.addUser(user1);
+	}
+	
+	@Test
+	public void addUserDuple(){
+		userDao.addUser(user1);
+		userDao.addUser(user1);
+	}
+
+}
