970053f20541f4dccc5ae8463c5f3533bd8682d3
Song Intae <Song@Songui-MacBook-Pro.local>
2017-05-09 17:44:20 +0900

    후

--- src/main/java/sobt/domain/user/User.java
+++ src/main/java/sobt/domain/user/User.java
@@ -3,6 +3,16 @@ package sobt.domain.user;
 public class User {
 	private String userId;
 	
+	public User(){
+		
+	}
+	public User(String userId){
+		this.userId = userId;
+	}
+	
+	public void setUserId(String userId){
+		this.userId = userId;
+	}
 	public String getUserId() {
 		return userId;
 	}
