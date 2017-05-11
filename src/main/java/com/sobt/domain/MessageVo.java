3ed7c60b093c562025d96a52788019c1947d5159
vangarang <vangarang@naver.com>
2017-05-11 21:52:25 +0900

    카카오톡-Spring 연동 완료 라우팅 목록 및 테스트 결과 : /keyboard   | test : OK 200 /message    | test : OK 200 /friend     | test : OK 200 /chat_room  | test : OK 200

--- src/main/java/com/sobt/domain/MessageVo.java
+++ src/main/java/com/sobt/domain/MessageVo.java
@@ -2,39 +2,59 @@ package com.sobt.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 /**
  * 응답 객체 ( Response object )
+ * 
  * @author namwoo
- * @method setKeyboard_buttons : 버튼 입력 방식  
+ * @method setKeyboard_buttons : 버튼 입력 방식
  */
 public class MessageVo {
 	@Autowired
 	Message message;
 	@Autowired
 	Keyboard keyboard;
-	
+
 	// ======== Spring 메소드 ==========
-	public Message getMessage() { return message; }
-	public void setMessage(Message message) { this.message = message; }
-	public Keyboard getKeyboard() { return keyboard; }
-	public void setKeyboard(Keyboard keyboard) { this.keyboard = keyboard; }
-	// ===============================	
-	
-	public void setText(String text){
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+
+	public Keyboard getKeyboard() {
+		return keyboard;
+	}
+
+	public void setKeyboard(Keyboard keyboard) {
+		this.keyboard = keyboard;
+	}
+	// ===============================
+
+	public void setText(String text) {
 		this.message.setText(text);
 	}
-	
-	public void setKeyboard_buttons(String button1){
+
+	public void setKeyboard_buttons(String button1) {
 		this.keyboard.setType("buttons");
 		this.keyboard.addButtons(button1);
 	}
-	public void setKeyboard_buttons(String button1, String button2){
+
+	public void setKeyboard_buttons(String button1, String button2) {
 		this.keyboard.setType("buttons");
 		this.keyboard.addButtons(button1, button2);
 	}
-	public void setKeyboard_buttons(String btn1, String btn2, String btn3){
+
+	public void setKeyboard_buttons(String btn1, String btn2, String btn3) {
 		this.keyboard.setType("buttons");
 		this.keyboard.addButtons(btn1, btn2, btn3);
+	}
+
+	public void setPhoto(String url, int width, int height) {
+		this.message.add_photo(url, width, height);
+	}
+	public void setMessageBtn(String label, String url){
+		this.message.add_messageBtn(label, url);
 	}
 }
--- src/main/java/com/sobt/project/MessageController.java
+++ src/main/java/com/sobt/project/MessageController.java
@@ -2,6 +2,7 @@ package com.sobt.project;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,18 +11,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.sobt.domain.MessageVo;
 import com.sobt.domain.User;
 
+import sobt.api.manage.WeatherApiManager;
+
 @Controller
 public class MessageController {
 
 	@Autowired
 	private MessageVo messageVo;
+	@Autowired
+	private WeatherApiManager weatehrApiManager;
 
 	@RequestMapping(value = "/message", method = RequestMethod.POST, headers = "Accept=application/json; charset=utf-8")
 	public @ResponseBody MessageVo message(@RequestBody User user) throws Exception {
 		String text = user.getContent();
 		messageVo.setText(text);
+		if (text.equals("날씨 정보")){
+			messageVo.setText(weatehrApiManager.getWeatherMin("서울", "노원구", "월계동"));
+		}else{
+			messageVo.setMessageBtn("github 바로가기", "https://github.com/SOBT/SOBT_SERVER");
+		}
 		messageVo.setKeyboard_buttons("날씨 정보", "지하철 정보", "영화 정보");
 		return messageVo;
 	}
-
 }
